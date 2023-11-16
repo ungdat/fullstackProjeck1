9527271d8e81ebe9cb8b9a851b41a9ce9aae35be
yukihitou <ztieu.linhz@gmail.com>
2023-11-16 15:09:54 +0700

    update database

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -5,9 +5,14 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "admin@gmail.com",
+        password: "123456",
+        firstName: "nhon124",
+        lastName: "dat",
+        address: "vietnam",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
